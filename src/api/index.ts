9d094fef448c578f4a29602024a0b624a8ffe850
Miikis <Miikis@users.noreply.github.com>
2020-01-19 23:16:17 -0800

    Refactor api base url into environment variable
    Signed-off-by: Miikis <Miikis@users.noreply.github.com>

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,13 +2,19 @@ import axios from "axios"
 
 import { OutcomesData, OutcomesInputData } from "../types"
 
+const { REACT_APP_FLASK_API_BASE_URL } = process.env
+
+const axiosInstance = axios.create({
+    baseURL: REACT_APP_FLASK_API_BASE_URL
+})
+
 export const fetchLocations = async () => {
-    const { data } = await axios.get("http://localhost:5000/locations")
+    const { data } = await axiosInstance.get("/locations")
     return data
 }
 
 export const fetchEthnicities = async () => {
-    const { data } = await axios.get("http://localhost:5000/ethnicities")
+    const { data } = await axiosInstance.get("/ethnicities")
     return data
 }
 
@@ -16,11 +22,8 @@ export const fetchOutcomes = async (
     location: string,
     inputData: OutcomesInputData
 ): Promise<OutcomesData> => {
-    const { data } = await axios.get(
-        `http://localhost:5000/outcomes/${location}`,
-        {
-            params: { ...inputData }
-        }
-    )
+    const { data } = await axiosInstance.get(`/outcomes/${location}`, {
+        params: { ...inputData }
+    })
     return data
 }
